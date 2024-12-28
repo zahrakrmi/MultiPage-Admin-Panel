c67b973835d74e43887e4b6455991c016dbbc6d2
zahra karami <69428157+zahrakrmi@users.noreply.github.com>
2024-12-28 22:51:13 +0330

    Add files via upload

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html suppressHydrationWarning lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
